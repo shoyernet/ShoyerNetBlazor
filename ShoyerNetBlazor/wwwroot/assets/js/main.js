d3d3bcf3aa7b75b219a47c324c5f08351175e0e4
Roy Shoyer <roy.shoyer@outlook.com>
2025-02-13 23:48:17 -0500

    * Google map fix

--- ShoyerNetBlazor/wwwroot/assets/js/main.js
+++ ShoyerNetBlazor/wwwroot/assets/js/main.js
@@ -6,15 +6,9 @@
 * License: https://bootstrapmade.com/license/
 */
 
-(g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
-    key: "AIzaSyBPGe4m9qssc05Eo-8w4Ucq22gL64oEsAo",
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-});
 
 // Initialize and add the map
-let map;
+var map;
 
 async function initMap() {
     const position = { lat: 43.638259956677025, lng: -79.41482508881865 };
@@ -36,7 +30,6 @@ async function initMap() {
     });
 }
 
-initMap();
 
 (function () {
     "use strict";
@@ -52,9 +45,6 @@ initMap();
     }
 
     document.addEventListener('scroll', toggleScrolled);
-    window.addEventListener('load', function () {
-        toggleScrolled();
-    });
 
     /**
      * Mobile nav toggle
@@ -98,8 +88,18 @@ initMap();
     const preloader = document.querySelector('#preloader');
     if (preloader) {
         window.addEventListener('load', () => {
-
             preloader.remove();
+
+            toggleScrolled();
+
+            (g => { var h, a, k, p = "The Google Maps JavaScript API", c = "google", l = "importLibrary", q = "__ib__", m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set, e = new URLSearchParams, u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement("script")); e.set("libraries", [...r] + ""); for (k in g) e.set(k.replace(/[A-Z]/g, t => "_" + t[0].toLowerCase()), g[k]); e.set("callback", c + ".maps." + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => h = n(Error(p + " could not load.")); a.nonce = m.querySelector("script[nonce]")?.nonce || ""; m.head.append(a) })); d[l] ? console.warn(p + " only loads once. Ignoring:", g) : d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)) })({
+                key: "AIzaSyBPGe4m9qssc05Eo-8w4Ucq22gL64oEsAo",
+                v: "weekly",
+                // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+                // Add other bootstrap parameters as needed, using camel case.
+            });
+
+            initMap();
         });
     }
 
