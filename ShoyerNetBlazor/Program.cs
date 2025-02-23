09c0bfd1525cc7cced8b410f9440bdb2840db4c5
Roy Shoyer <roy.shoyer@outlook.com>
2025-02-22 22:16:03 -0500

    * Added signalr

--- ShoyerNetBlazor/Program.cs
+++ ShoyerNetBlazor/Program.cs
@@ -1,30 +1,5 @@
-﻿using ShoyerNetBlazor.Components;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorComponents()
-//    .AddInteractiveServerComponents();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-
-//app.UseStaticFiles();
-//app.UseAntiforgery();
-
-//app.MapRazorComponents<App>()
-//    .AddInteractiveServerRenderMode();
-
-//app.Run();
+﻿using ShoyerNetBlazor.Common.Hubs;
+using ShoyerNetBlazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +8,8 @@ builder.Services.AddControllers();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -57,6 +34,9 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+
+app.MapHub<ChatHub>("/chathub");
 
 //var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 //app.Urls.Add($"http://0.0.0.0:{port}");
