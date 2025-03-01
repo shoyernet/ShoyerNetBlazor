543ddf9f94dcef0437ee7f5fd815ca3cf737d6f5
Roy Shoyer <roy.shoyer@outlook.com>
2025-03-01 17:21:20 -0500

    * Added contactus form

---
+++ ShoyerNetBlazor/Models/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoyerNetBlazor.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(500, ErrorMessage = "Message cannot exceed 500 characters")]
+        public string Message { get; set; } = string.Empty;
+    }
+}
